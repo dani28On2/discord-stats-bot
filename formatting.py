be343d540ade6830570b340e03f2e8b9d3d9938c
Skibidi Boy <danipastorbellver@gmail.com>
2026-06-23 21:23:26 +0200

    Reorganize ABBREV list and update format_value function

--- formatting.py
+++ formatting.py
@@ -25,10 +25,26 @@ from decimal import Decimal, InvalidOperation
 # Orden EXACTO del juego. NO reordenar: la posición es la magnitud.
 ABBREV: list[str] = [
     "", "K", "M", "B", "T",
-    "Qa", "Qi", "Sx", "Sp", "Oc",
-    "No", "Dc", "Un", "Du", "Tr",
-    "Qt", "Qn", "Se", "St", "Og",
-    "Nn", "Vg", "UVg",
+    "Qa", "Qt", "Sx", "Sp", "Oc",
+    "No", "Dc", "UDc", "DDc", "TDc",
+    "QaDc", "QtDc", "SxDc", "SpDc", "ODc",
+    "NDc", "Vg", "UVg", "DVg", "TVg",
+    "QaVg", "QtVg", "SxVg", "SpVg", "OVg",
+    "NVg", "Tg", "UTg", "DTg", "TTg",
+    "QaTg", "QtTg", "SxTg", "SpTg", "OTg",
+    "NTg", "Qd", "UQd", "DQd", "TQd",
+    "QaQd", "QtQd", "SxQd", "SpQd", "OQd",
+    "NQd", "Qi", "UQi", "DQi", "TQi",
+    "QaQi", "QtQi", "SxQi", "SpQi", "OQi",
+    "NQi", "Se", "USe", "DSe", "TSe",
+    "QaSe", "QtSe", "SxSe", "SpSe", "OSe",
+    "NSe", "St", "USt", "DSt", "TSt",
+    "QaSt", "QtSt", "SxSt", "SpSt", "OSt",
+    "NSt", "Og", "UOg", "DOg", "TOg",
+    "QaOg", "QtOg", "SxOg", "SpOg", "OOg",
+    "NOg", "Nn", "UNn", "DNn", "TNn",
+    "QaNn", "QtNn", "SxNn", "SpNn", "ONn",
+    "NNn", "Dd", "DDd",
 ]
 
 # Diccionario sufijo -> exponente (mil^i = 10^(3*i)).
